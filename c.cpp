7e0fdacb7f2a233b443a7496ae9499cb3789deb3
Pradeep4157 <150575206+Pradeep4157@users.noreply.github.com>
2024-11-06 01:06:33 +0530

    c.cpp

--- c.cpp
+++ c.cpp
@@ -1,25 +1,9 @@
 #include <bits/stdc++.h>
 #define ll int64_t
 using namespace std;
-vector<ll>primenumbers(int n){
-    vector<ll>res;
-    vector<bool>isprime(n+1,true);
-    isprime[0]=false;
-    isprime[1]=false;
-    for(int i=2;i*i<=n;i++){
-        if(isprime[i]){
-            for(int j=i*i;j<=n;j+=i){
-                isprime[j]=false;
-            }
-        }
-    }
-    for(int i=2;i<=n;i++){
-        if(isprime[i]) res.push_back(i);
-    }
-    return res;
-}
 bool isprime(ll n){
-    for(int i=2;i<n;i++) if(n%i==0) return false;
+    if(n==1) return false;
+    for(int i=2;i*i<=n;i++) if(n%i==0) return false;
     return true;
 }   
 
@@ -30,7 +14,7 @@ int main() {
     while(t--){
         int m,n;
         cin>>m>>n;
-        for(ll i=m;i<n;i++){
+        for(ll i=m;i<=n;i++){
             if(isprime(i)) cout<<i<<endl;
         }
         cout<<endl;
